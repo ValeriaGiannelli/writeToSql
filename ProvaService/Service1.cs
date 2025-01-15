28bc796a0eeec28547637e180b694548c6dc93d5
vale.giannelli <vg.giannellivaleria@gmail.com>
2025-01-15 15:43:33 +0100

    Add project files.

---
+++ ProvaService/Service1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using SQLServiceLibrary;
+
+namespace WriteToSql
+{
+    public partial class Service1 : ServiceBase
+    {
+
+        //creo variabile timer
+        Timer timer = new Timer(); //usa system.Timers
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+
+        //****************START SERVIZIO*************//
+        protected override void OnStart(string[] args)
+        {
+            //richiama il metodo per scrivere nel file che il servizio è iniziato all'ora X
+            WriteToFile($"Servizio iniziato alle {DateTime.Now}");
+
+            //richiama il metodo per scrivere nel file che il servizio è iniziato all'ora X
+            WriteToDB.WriteToDatabase($"Servizio iniziato alle {DateTime.Now}");
+
+            //ogni volta che l'evento viene generato fa il metodo OnElapsedTime che stampa qualcosa
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000; //dopo quanto fa ripartire l'evento
+            timer.Enabled = true;
+        }
+
+        
+        /// <summary>
+        /// METODO SCRITTURA FILE (Scrive nel file il messaggio che viene passato)
+        /// </summary>
+        /// <param name="message">Messaggio di log</param>
+        private void WriteToFile(string message)
+        {
+            //crea il percorso prendendo il dominio e la cd
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"\Logs";
+
+            //controlla che il percorso esista-> se non esiste lo crea.
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            //specifico il percorso del file - esempio output: C:\MyApp\Logs\ServiceLog_14_01_2025.txt
+            string filepath = path + @"\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+
+            //se non esiste crea il file dove scrivere
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else //se esiste aggiunge  il messaggio al file
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+
+        //METODO AD OGNI CICLO (richiama funzione di scrittura nel file e db)
+        private void OnElapsedTime(object sender, ElapsedEventArgs e)
+        {
+            WriteToFile("Il servizio è stato richiamato alle " + DateTime.Now);
+            WriteToDB.WriteToDatabase("Il servizio è stato richiamato alle " + DateTime.Now);
+        }
+
+
+        //*****************STOP SERVIZIO******************//
+        protected override void OnStop()
+        {
+            WriteToFile("il servizio si è fermato alle: " + DateTime.Now);
+            WriteToDB.WriteToDatabase("il servizio si è fermato alle: " +  DateTime.Now);
+        }
+    }
+}
---
+++ SQLServiceLibrary/WriteToDB.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLServiceLibrary
+{
+    public class WriteToDB
+    {
+        /// <summary>
+        /// METODO SCRITTURA DB (Si connette al DB e inserisce in tabella i dati)
+        /// </summary>
+        /// <param name="message"></param>
+        public static void WriteToDatabase(string message)
+        {
+            //crea il percorso prendendo il dominio e la cd (cambiare i valori)
+            string connectionString = "Server=nome_server;Database=nome_db;Trusted_Connection=True;";
+
+            //query per inserire i dati nella tabella
+            string query = "INSERT INTO service_logs (Message, LoggedAt) VALUES (@Message, @LoggedAt)";
+
+            //fai la connessione a SQL con la stringa data e fai al query
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    //apre la connessione
+                    connection.Open();
+                    Console.WriteLine("Connessione riuscita");
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        //aggiungi parametri
+                        command.Parameters.AddWithValue("@Message", message);
+                        command.Parameters.AddWithValue("@LoggedAt", DateTime.Now);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Eventuale gestione dell'errore (ad esempio log nel file in caso di errore)
+                File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + @"\ErrorLog.txt",
+                    $"Errore: {ex.Message} - {DateTime.Now}\n");
+                Console.WriteLine("Errore di connessione " + ex.Message);
+            }
+        }
+    }
+}
