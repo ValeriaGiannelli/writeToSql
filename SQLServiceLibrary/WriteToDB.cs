de28bd3a22dee8cec90f45143cbc8ec8e698aa82
vale.giannelli <vg.giannellivaleria@gmail.com>
2025-01-16 09:20:11 +0100

    feat: add second service to Delete the first 5 rows in the table

--- SQLServiceLibrary/WriteToDB.cs
+++ SQLServiceLibrary/WriteToDB.cs
@@ -17,7 +17,7 @@ namespace SQLServiceLibrary
         public static void WriteToDatabase(string message)
         {
             //crea il percorso prendendo il dominio e la cd (cambiare i valori)
-            string connectionString = "Server=nome_server;Database=nome_db;Trusted_Connection=True;";
+            string connectionString = "Server=localhost;Database=prova_sql;Trusted_Connection=True;";
 
             //query per inserire i dati nella tabella
             string query = "INSERT INTO service_logs (Message, LoggedAt) VALUES (@Message, @LoggedAt)";
@@ -47,6 +47,45 @@ namespace SQLServiceLibrary
                 File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + @"\ErrorLog.txt",
                     $"Errore: {ex.Message} - {DateTime.Now}\n");
                 Console.WriteLine("Errore di connessione " + ex.Message);
+            }
+        }
+    }
+
+    public class DeleteFromDB
+    {
+        /// <summary>
+        /// METODO SCRITTURA DB (Si connette al DB e inserisce in tabella i dati)
+        /// </summary>
+        /// <param name="message"></param>
+        public static void DeleteFromDatabase()
+        {
+            //crea il percorso prendendo il dominio e la cd (cambiare i valori)
+            string connectionString = "Server=localhost;Database=prova_sql;Trusted_Connection=True;";
+
+            //query per inserire i dati nella tabella
+            string query = "DELETE FROM service_logs WHERE Id IN (SELECT TOP 5 Id FROM service_logs ORDER BY Id)";
+
+            //fai la connessione a SQL con la stringa data e fai al query
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    //apre la connessione
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        int rowsAffected = command.ExecuteNonQuery(); // Esegue la query di eliminazione
+                        Console.WriteLine($"{rowsAffected} righe eliminate con successo.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Eventuale gestione dell'errore
+                string errorPath = AppDomain.CurrentDomain.BaseDirectory + @"\ErrorLog.txt";
+                File.AppendAllText(errorPath, $"Errore: {ex.Message} - {DateTime.Now}\n");
+                Console.WriteLine($"Errore di connessione: {ex.Message}");
             }
         }
     }
